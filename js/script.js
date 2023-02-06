e84060411123e9c7347bd2e740a864ca988820a2
unknown <byiringoroscar@gmail.com>
2023-02-06 23:06:17 +0200

    fix linters errors

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-/* eslint-disable quotes */
 window.onload = () => {
-  const mobileBtn = document.querySelector(".mob-icon");
-  const mobileBtnClose = document.querySelector(".close-btn");
-  const mobileBody = document.querySelector(".mobile-body");
-  const navMain = document.querySelector(".main-nav");
+  const mobileBtn = document.querySelector('.mob-icon');
+  const mobileBtnClose = document.querySelector('.close-btn');
+  const mobileBody = document.querySelector('.mobile-body');
+  const navMain = document.querySelector('.main-nav');
   // const mainLogo = document.querySelector(".main-logo");
-  mobileBtn.addEventListener("click", () => {
-    mobileBtn.classList.remove("is-active");
-    mobileBtnClose.classList.add("is-active");
-    mobileBody.classList.add("is-active");
+  mobileBtn.addEventListener('click', () => {
+    mobileBtn.classList.remove('is-active');
+    mobileBtnClose.classList.add('is-active');
+    mobileBody.classList.add('is-active');
     // mobileBody.style.transition = "all 3s";
-    navMain.style.position = "fixed";
-    document.querySelector(".logo-link").style.display = "none";
+    navMain.style.position = 'fixed';
+    document.querySelector('.logo-link').style.display = 'none';
   });
-  mobileBtnClose.addEventListener("click", () => {
-    mobileBtn.classList.add("is-active");
-    mobileBtnClose.classList.remove("is-active");
-    mobileBody.classList.remove("is-active");
-    navMain.style.position = "static";
-    document.querySelector(".logo-link").style.display = "block";
+  mobileBtnClose.addEventListener('click', () => {
+    mobileBtn.classList.add('is-active');
+    mobileBtnClose.classList.remove('is-active');
+    mobileBody.classList.remove('is-active');
+    navMain.style.position = 'static';
+    document.querySelector('.logo-link').style.display = 'block';
   });
 };
