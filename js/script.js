02281c7205980be2195414124a4f6edcd8e7dc99
unknown <byiringoroscar@gmail.com>
2023-02-07 08:16:08 +0200

    fix humber btn disapear, linters and font-family

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ window.onload = () => {
   const mobileBtnClose = document.querySelector('.close-btn');
   const mobileBody = document.querySelector('.mobile-body');
   const navMain = document.querySelector('.main-nav');
-  // const mainLogo = document.querySelector(".main-logo");
   mobileBtn.addEventListener('click', () => {
     mobileBtn.classList.remove('is-active');
     mobileBtnClose.classList.add('is-active');
     mobileBody.classList.add('is-active');
-    // mobileBody.style.transition = "all 3s";
     navMain.style.position = 'fixed';
     document.querySelector('.logo-link').style.display = 'none';
   });
